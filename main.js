545e20c4229e0e1059e36bd055adb8f865cda2c2
fabio98p <parisifabio98@gmail.com>
2021-03-11 20:48:04 +0100

    carousel v1.1 aggiornamento con le funzioni

--- main.js
+++ main.js
@@ -3,45 +3,56 @@ console.log("ciao ale");
 var prev = $(".slider-wrapper .prev i");
 var next = $(".slider-wrapper .next i");
 
-//inizio la funzione next alla pressione del pulsante
+//#region per il funzionamento con le frecciette
 next.click(function () {
+	nextElement(".images", "img");
+})
+prev.click(function () {
+	prevElement(".images", "img");
+})
+next.click(function () {
+	nextElement(".nav", "i");
+})
+prev.click(function () {
+	prevElement(".nav", "i");
+})
+//#endregion
+
+function nextElement(container, child) {
 	//inizializzo qua la variabile active 
-	var imgActive = $(".images img.active");
+	var active = $(`${container} ${child}.active`);
 
 	//se l'immagine ha la classe last vuol dire che si trova alla sua ultima foto
-	if(imgActive.hasClass("last")){
+	if(active.hasClass("last")){
 		//cambio delle classi active tra l'ultima foto e la prima
-		var previusActive = $(".images img.first")
-		imgActive.removeClass("active");
+		var previusActive = $(`${container} ${child}.first`);
+		active.removeClass("active");
 		previusActive.addClass("active");
 	}
 	//altrimenti scrollera le fono normalmente con next
 	else{
 		//cambio delle classi active quella attuale e la successiva
-		var previusActive = imgActive.next()
-		imgActive.removeClass("active");
+		var previusActive = active.next();
+		active.removeClass("active");
 		previusActive.addClass("active");
 	}
-	imgActive.removeClass("active");
-})
+}
+function prevElement(container, child) {
+	//inizializzo qua la variabile active 
+	var active = $(`${container} ${child}.active`);
 
-prev.click(function () {
-	var imgActive = $(".images img.active");
-
-	//se l'immagine ha la classe first vuol dire che si trova alla sua prima foto
-	if(imgActive.hasClass("first")){
-		//cambio delle classi active tra l'ultima foto e la prima
-		var nextActive = $(".images img.last")
-		imgActive.removeClass("active");
-		nextActive.addClass("active");
+	//se l'immagine ha la classe last vuol dire che si trova alla sua prima foto
+	if(active.hasClass("first")){
+		//cambio delle classi active tra l'prima foto e la ultima
+		var previusActive = $(`${container} ${child}.last`);
+		active.removeClass("active");
+		previusActive.addClass("active");
 	}
 	//altrimenti scrollera le fono normalmente con prev
 	else{
-		//cambio delle classi active quella attuale e la precedente
-		var nextActive = imgActive.prev()
-		imgActive.removeClass("active");
-		nextActive.addClass("active");
+		//cambio delle classi active quella attuale e la successiva
+		var previusActive = active.prev();
+		active.removeClass("active");
+		previusActive.addClass("active");
 	}
-})
-
-
+}
